dc7cf1868ca681d83f9128bf69da0e2bc2f6907b
Lucas Barrenechea <lucascode07@gmail.com>
2023-09-06 02:13:21 -0500

    feat: se agrega sección de nuestros servicios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import MainSection from "@/components/organism/main-section/main-section";
+import ServicesSection from "@/components/organism/section-services/section-services";
 import { getHomePageData } from "@/services/contentful-queries";
 
 export default async function Home() {
 
-  const mainSectionData = await getHomePageData();
+  const { mainSection, servicesSection } = await getHomePageData();
 
   return (
     <main>
-      <MainSection sectionData={mainSectionData} />
+      <MainSection sectionData={mainSection} />
+      <ServicesSection sectionData={servicesSection} />
     </main>
   )
 }
